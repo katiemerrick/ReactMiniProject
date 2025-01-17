c8a6487b48bc2efd824067ee56f55fbccdd7ff8e
Ben <bencook1023@gmail.com>
2025-01-16 21:41:33 -0500

    modified app.jsx, main.jsx, dictionaryselector.jsx

--- src/components/DictionarySelector.jsx
+++ src/components/DictionarySelector.jsx
@@ -1,60 +1,35 @@
-import { Link, useLocation } from 'react-router-dom';
-
+// import { Link, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from react
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function DictionarySelector() {
-  const currentPage = useLocation().pathname;
+    const [dictionaries, setDictionaries] = useState('');
+    useEffect(() => {
+        fetch('https://1rnoszgn46.execute-api.us-east-1.amazonaws.com/multi-details')
+        .then(response=> response.json())
+        .then ((data) => {
+            console.log(data.dictionaries)
+            setDictionaries(data.dictionaries)
+        })
+    })
 
-  return (
-    <ul className="nav nav-tabs">
-      <li className="selector-item">
-        <Link
-          to="/"
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'selector-link-active', otherwise we set it to 'selector-link '
-          className={currentPage === '/' ? 'selector-link active' : 'selector-link '}
-        >
-          Home
-        </Link>
-      </li>
-      <li className="selector-item">
-        <Link
-          to="/GeneralBiology"
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal selector-link 
-          className={currentPage === '/GeneralBiology' ? 'selector-link active' : 'selector-link '}
-        >
-          General Biology
-        </Link>
-      </li>
-      <li className="selector-item">
-        <Link
-          to="/GeneralChemistry"
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal selector-link 
-          className={currentPage === '/GeneralChemistry' ? 'selector-link active' : 'selector-link '}
-        >
-          General Chemistry
-        </Link>
-      </li>
-      <li className="selector-item">
-        <Link
-          to="/GeneralGeology"
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal selector-link 
-          className={currentPage === '/GeneralGeology' ? 'selector-link active' : 'selector-link '}
-        >
-          General Geology
-        </Link>
-      </li>
-      <li className="selector-item">
-        <Link
-          to="/MedicalTerms"
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal selector-link 
-          className={currentPage === '/MedicalTerms' ? 'selector-link active' : 'selector-link '}
-        >
-          Medical Terms
-        </Link>
-      </li>
-    </ul>
-  );
+    return (
+        <div>
+            <h1>
+                Please select a dictionary
+            </h1>
+            {
+            dictionaries.map((dictionary , index) => (
+                <button>
+                    {dictionary.title}
+                    {dictionary.tags}
+                    {index}
+                </button>
+            ))
+            }
+        </div>
+
+    );
 }
 
 export default DictionarySelector;
--- src/main.jsx
+++ src/main.jsx
@@ -5,40 +5,25 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App';
-import Error from './pages/Error';
-import Home from './pages/Home';
-import GeneralBiology from './pages/GeneralBiology';
-import GeneralChemistry from './pages/GeneralChemistry';
-import MedicalTerms from './pages/MedicalTerms';
-import GeneralGeology from './pages/GeneralGeology';
+// import Error from './pages/Error';
+// import Home from './pages/Home';
+import DictionarySelector from './components/DictionarySelector';
 
 // Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <Error />,
+    // errorElement: <Error />,
     children: [
       {
         index: true,
-        element: <Home />,
+        element: <DictionarySelector />,
       },
-      {
-        path: '/GeneralBiology',
-        element: <GeneralBiology />,
-      },
-      {
-        path: '/GeneralChemistry',
-        element: <GeneralChemistry />,
-      },
-      {
-        path: '/GeneralGeology',
-        element: <GeneralGeology />,
-      },
-      {
-        path: '/MedicalTerms',
-        element: <MedicalTerms/>,
-      },
+      // {
+      //   path: '/GeneralBiology',
+      //   element: <GeneralBiology />,
+      // },
     ],
   },
 ]);
